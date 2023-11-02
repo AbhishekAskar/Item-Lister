e06f587ec12f1db430c53d4b829cf31c3d2db474
AbhishekAskar <abhiaskar.18@gmail.com>
2023-11-02 22:36:18 +0530

    second commit

---
+++ dom.js
@@ -0,0 +1,5 @@
+let header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+let addItem = document.querySelector('h2.title');
+addItem.style.fontWeight = 'bold';
+addItem.style.color = 'green';
