fa76fed8f169088242b99578c256dd6701379624
Abhishek <abhiaskar.18@gmail.com>
2023-11-03 11:04:29 +0530

    fourth commit

--- dom.js
+++ dom.js
@@ -1,5 +1,16 @@
+// using getElementById
 let header = document.getElementById('main-header');
 header.style.borderBottom = 'solid 3px #000';
+
+// using querySelector
 let addItem = document.querySelector('h2.title');
 addItem.style.fontWeight = 'bold';
 addItem.style.color = 'green';
+
+
+// using getElementsByClassName
+let items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'green';
+for(let i = 0; i < items.length; i++){
+    items[i].style.fontWeight = 'bold';
+}
